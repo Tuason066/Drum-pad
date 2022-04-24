041f2d9e55c51191318703a2a2c8ee68d8354378
Jeffrey Tuason <jepoyduque06@gmail.com>
2022-04-24 14:05:58 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+
+function playPad(e) {
+    const key = e.keyCode;
+
+    const btn = document.querySelector(`.pad[data-key="${key}"]`);
+    if(!btn) return;
+    btn.classList.add('pressed');
+
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    if(!audio) {return};
+    audio.currentTime = 0;
+    audio.play();
+    
+};
+
+function playAudio() {
+    const key = parseInt(this.dataset.key);
+    
+    const btn = document.querySelector(`.pad[data-key="${key}"]`);
+    if(!btn) return;
+    btn.classList.add('pressed');
+
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    if(!audio) {return};
+    audio.currentTime = 0;
+    audio.play();
+    
+};
+
+function removeTransition(e) {
+    this.classList.remove('pressed');
+};
+
+const pads = document.querySelectorAll('.pad');
+pads.forEach(pad => pad.addEventListener('transitionend', removeTransition));
+
+pads.forEach(pad => pad.addEventListener('click', playAudio));
+
+window.addEventListener('keydown', playPad);
