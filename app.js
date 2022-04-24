50511fb9a6d93a995ed327b1923d66ec45c72f31
Jeffrey Tuason <jepoyduque06@gmail.com>
2022-04-24 16:52:32 +0800

    code refactoring

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-
-function playPad(e) {
-    const key = e.keyCode;
-
-    const btn = document.querySelector(`.pad[data-key="${key}"]`);
-    if(!btn) return;
-    btn.classList.add('pressed');
-
-    const audio = document.querySelector(`audio[data-key="${key}"]`);
-    if(!audio) {return};
-    audio.currentTime = 0;
-    audio.play();
+class UI {
+    static playAudio() {
+        const key = parseInt(this.dataset.key);
     
-};
+        const btn = document.querySelector(`.pad[data-key="${key}"]`);
+        if(!btn) return;
+        btn.classList.add('pressed');
 
-function playAudio() {
-    const key = parseInt(this.dataset.key);
-    
-    const btn = document.querySelector(`.pad[data-key="${key}"]`);
-    if(!btn) return;
-    btn.classList.add('pressed');
+        const audio = document.querySelector(`audio[data-key="${key}"]`);
+        if(!audio) {return};
+        audio.currentTime = 0;
+        audio.play();
+    };
 
-    const audio = document.querySelector(`audio[data-key="${key}"]`);
-    if(!audio) {return};
-    audio.currentTime = 0;
-    audio.play();
-    
-};
+    static playPad(e) {
+        const key = e.keyCode;
 
-function removeTransition(e) {
-    this.classList.remove('pressed');
+        const btn = document.querySelector(`.pad[data-key="${key}"]`);
+        if(!btn) return;
+        btn.classList.add('pressed');
+
+        const audio = document.querySelector(`audio[data-key="${key}"]`);
+        if(!audio) {return};
+        audio.currentTime = 0;
+        audio.play();
+    };
+
+    static removeTransition() {
+        this.classList.remove('pressed');
+    }
 };
 
 const pads = document.querySelectorAll('.pad');
-pads.forEach(pad => pad.addEventListener('transitionend', removeTransition));
+pads.forEach(pad => pad.addEventListener('transitionend', UI.removeTransition));
+pads.forEach(pad => pad.addEventListener('touchend', UI.playAudio));
+window.addEventListener('keydown', UI.playPad);
 
-pads.forEach(pad => pad.addEventListener('click', playAudio));
-pads.forEach(pad => pad.addEventListener('touchend', playAudio));
-
-window.addEventListener('keydown', playPad);
+window.addEventListener('resize', () => {
+    const screenWidth = window.innerWidth;
+    if(screenWidth > 768) {
+        pads.forEach(pad => pad.addEventListener('click', UI.playAudio));
+    };
+});
